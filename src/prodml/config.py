763a0552c308881cbe7ad3fa9ef8ea52ce23bba4
OmarAmir2001 <omaramir2001@gmail.com>
2026-08-19 20:54:28 +0300

    Refactor code structure for improved readability and maintainability

---
+++ src/prodml/config.py
@@ -0,0 +1,18 @@
+# config.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    NAME: str
+    VERSION: str
+
+    MODEL_FILE: str
+    CHURN_THRESHOLD: float = 0.5
+
+    class Config:
+        env_file = ".env"
+
+
+def get_settings() -> Settings:
+    return Settings()
---
+++ src/prodml/data_schema.py
@@ -0,0 +1,56 @@
+# api/schema.py
+from typing import Literal
+from pydantic import BaseModel, Field
+
+
+class Customer(BaseModel):
+    gender: Literal['female', 'male']
+    seniorcitizen: int = Field(ge=0, le=1)
+    partner: Literal['yes', 'no']
+    dependents: Literal['yes', 'no']
+    phoneservice: Literal['yes', 'no']
+    multiplelines: Literal['yes', 'no', 'no_phone_service']
+    internetservice: Literal['dsl', 'fiber_optic', 'no']
+    onlinesecurity: Literal['yes', 'no', 'no_internet_service']
+    onlinebackup: Literal['yes', 'no', 'no_internet_service']
+    deviceprotection: Literal['yes', 'no', 'no_internet_service']
+    techsupport: Literal['yes', 'no', 'no_internet_service']
+    streamingtv: Literal['yes', 'no', 'no_internet_service']
+    streamingmovies: Literal['yes', 'no', 'no_internet_service']
+    contract: Literal['month-to-month', 'one_year', 'two_year']
+    paperlessbilling: Literal['yes', 'no']
+    paymentmethod: Literal[
+        'electronic_check',
+        'mailed_check',
+        'bank_transfer_(automatic)',
+        'credit_card_(automatic)',
+    ]
+    tenure: int = Field(ge=0, le=100)
+    monthlycharges: float = Field(gt=0)
+    totalcharges: float = Field(ge=0)
+
+    model_config = {
+        'json_schema_extra': {
+            'example': {
+                'gender': 'female',
+                'seniorcitizen': 0,
+                'partner': 'yes',
+                'dependents': 'no',
+                'phoneservice': 'no',
+                'multiplelines': 'no_phone_service',
+                'internetservice': 'dsl',
+                'onlinesecurity': 'no',
+                'onlinebackup': 'yes',
+                'deviceprotection': 'no',
+                'techsupport': 'no',
+                'streamingtv': 'no',
+                'streamingmovies': 'no',
+                'contract': 'month-to-month',
+                'paperlessbilling': 'yes',
+                'paymentmethod': 'electronic_check',
+                'tenure': 1,
+                'monthlycharges': 29.85,
+                'totalcharges': 29.85,
+            }
+        }
+    }
